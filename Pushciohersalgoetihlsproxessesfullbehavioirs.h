d9d3936e2f34d10ce98bcb1f291a8297c95d54d6
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-12-12 09:12:38 +0100

    Create Pushciohersalgoetihlsproxessesfullbehavioirs.h

---
+++ Pushciohersalgoetihlsproxessesfullbehavioirs.h
@@ -0,0 +1,5 @@
+//ciphers are used for push commands according
+//to human machine interactiok or ring sub systems interactions 
+//there exist. then the far away values of ciphers of large scale networks
+//cannot handle push commands. so it is best to have a ring hzre too for a decade time.
+//tonreduce this time, it existe 10 minutes which is a tezhold hold by a random cipher.
