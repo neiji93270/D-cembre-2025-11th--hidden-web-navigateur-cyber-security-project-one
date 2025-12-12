f70c5373ae796da49e45ad1fc21c67ba3d1b73bc
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-12-12 07:47:55 +0100

    Create Ringpoweredvirtualotherrings.h

---
+++ Ringpoweredvirtualotherrings.h
@@ -0,0 +1,3 @@
+//the base project in readme was states by rings behaviour.
+//sonwe can negate those behaviour if the complex system or subsystems are stated in rings.
+//this is some sors of commit behaviour.
